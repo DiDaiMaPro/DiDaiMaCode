f8e63c9f61abd092a6b378a4902d504ff98b94dd
Davices <85142497+Davices@users.noreply.github.com>
2021-07-18 10:30:13 +0800

    Update UserEntity.cs

--- ExpressCode.Model/Admin/UserEntity.cs
+++ ExpressCode.Model/Admin/UserEntity.cs
@@ -11,5 +11,6 @@ namespace ExpressCode.Model
         public string name { get; set; }
         public string sex { get; set; }
         public string age { get; set; }
+        public string age22 { get; set; }
     }
 }
